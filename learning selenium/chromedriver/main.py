89f91d91b66c1b225abc048d1cd4425d5356e49f
ordenmeny <hogvarts06@mail.ru>
2023-05-17 19:17:42 +0500

    learning selenium: работа с proxy

--- learning selenium/chromedriver/main.py
+++ learning selenium/chromedriver/main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from fake_useragent import UserAgent
+# pip install selenium-wire
+from seleniumwire import webdriver
 
 # Объект fake_useragent для создания фейковых useragent
 useragent = UserAgent()
@@ -13,18 +15,28 @@ options.add_argument(
     f"user-agent={useragent.random}"
 )
 
+# set proxy
+options.add_argument('--proxy-server=95.217.183.24:8080')
+
+# proxy_options = {
+#     'proxy': {
+#         'https': f'http://{login}:{password}@159.223.137.96:8080'
+#     }
+# }
+
 # driver
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()),
+    options=options,
+    # seleniumwire_options=proxy_options
+)
 
 # url
 url = "https://whatmyuseragent.com/"
 
 try:
-    driver.get(url=url)
-    time.sleep(3)
-    # driver.refresh()
-    # driver.get_screenshot_as_file("1.png")
-    # driver.save_screenshot("2.png")
+    driver.get('https://whoer.net/ru')
+    time.sleep(60)
 except Exception as ex:
     print(ex)
 finally:
